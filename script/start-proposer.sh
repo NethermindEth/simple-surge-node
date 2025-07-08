153db59364c5f79a65870695edaa6e7bb60a2a44
Diptanshu Kakwani <dipkakwani@gmail.com>
2025-07-08 10:32:45 +0530

    Fix Blob Env Variable Names (#18)
    * fix blob env variable names
    
    * remove existing duplicates in script/start-proposer.sh

--- script/start-proposer.sh
+++ script/start-proposer.sh
@@ -52,30 +52,6 @@ if [ "$ENABLE_PROPOSER" = "true" ]; then
         ARGS="${ARGS} --surge.priceFluctuationModifier ${SURGE_PRICE_FLUCTUATION_MODIFIER}"
     fi
 
-    if [ -n "$CHECK_PROFITABILITY" ]; then
-        ARGS="${ARGS} --checkProfitability=${CHECK_PROFITABILITY}"
-    fi
-
-    if [ -n "$ALLOW_EMPTY_BLOCKS" ]; then
-        ARGS="${ARGS} --allowEmptyBlocks=${ALLOW_EMPTY_BLOCKS}"
-    fi
-
-    if [ -n "$SURGE_PROPOSING_BLOCK_GAS" ]; then
-        ARGS="${ARGS} --surge.gasNeededForProposingBlock ${SURGE_PROPOSING_BLOCK_GAS}"
-    fi
-
-    if [ -n "$SURGE_PROVING_BLOCK_GAS" ]; then
-        ARGS="${ARGS} --surge.gasNeededForProvingBlock ${SURGE_PROVING_BLOCK_GAS}"
-    fi
-
-    if [ -n "$SURGE_OFF_CHAIN_COSTS" ]; then
-        ARGS="${ARGS} --surge.offChainCosts ${SURGE_OFF_CHAIN_COSTS}"
-    fi
-
-    if [ -n "$SURGE_PRICE_FLUCTUATION_MODIFIER" ]; then
-        ARGS="${ARGS} --surge.priceFluctuationModifier ${SURGE_PRICE_FLUCTUATION_MODIFIER}"
-    fi
-
     if [ -n "$EPOCH_INTERVAL" ]; then
         ARGS="${ARGS} --epoch.interval ${EPOCH_INTERVAL}"
     fi
@@ -105,11 +81,11 @@ if [ "$ENABLE_PROPOSER" = "true" ]; then
         ARGS="${ARGS} --txPool.locals ${TXPOOL_LOCALS}"
     fi
 
-    if [ "$BLOB_ALLOWED" == "true" ]; then
+    if [ "$L1_BLOB_ALLOWED" == "true" ]; then
         ARGS="${ARGS} --l1.blobAllowed"
     fi
 
-    if [ "$FALLBACK_TO_CALLDATA" == "true" ]; then
+    if [ "$L1_FALLBACK_TO_CALLDATA" == "true" ]; then
         ARGS="${ARGS} --l1.fallbackToCalldata"
     fi
 
