cc6c5db4db6632c03ef1e44a66881f38dd3ee502
Justin Chan <justin.chan@nethermind.io>
2025-07-31 19:57:49 +1000

    refac: update scripts to use correct verifier addresses and deposit bond only to proposer wrapper

--- deployer/deposit-bond.sh
+++ deployer/deposit-bond.sh
@@ -6,4 +6,4 @@ set -e
 
 cast send ${SURGE_PROPOSER_WRAPPER} 'depositBond(uint256)' ${BOND_AMOUNT} --value ${BOND_AMOUNT} --private-key ${PRIVATE_KEY} --rpc-url ${L1_ENDPOINT_HTTP} --gas-limit ${GAS_LIMIT}
 
-cast send ${TAIKO_INBOX} 'depositBond(uint256)' ${BOND_AMOUNT} --value ${BOND_AMOUNT} --private-key ${PRIVATE_KEY} --rpc-url ${L1_ENDPOINT_HTTP} --gas-limit ${GAS_LIMIT}
+# cast send ${TAIKO_INBOX} 'depositBond(uint256)' ${BOND_AMOUNT} --value ${BOND_AMOUNT} --private-key ${PRIVATE_KEY} --rpc-url ${L1_ENDPOINT_HTTP} --gas-limit ${GAS_LIMIT}
