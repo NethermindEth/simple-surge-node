6a2c671cf16ae9166513b704bbd4ef8bb7a89089
Nurbakhyt Madibekov <jmadibekov@gmail.com>
2026-02-09 05:45:33 +0000

    Add fixes

--- script/start-prover-relayer.sh
+++ script/start-prover-relayer.sh
@@ -7,6 +7,8 @@ if [ "$ENABLE_PROVER" = "true" ]; then
         --l1.ws ${L1_ENDPOINT_WS}
         --l2.ws ws://l2-nethermind-execution-client:${L2_WS_PORT}
         --l2.http http://l2-nethermind-execution-client:${L2_HTTP_PORT}
+        --l2.auth http://l2-nethermind-execution-client:${L2_ENGINE_API_PORT}
+        --jwtSecret /tmp/jwt/jwtsecret
         --shastaInbox ${SHASTA_SURGE_INBOX}
         --pacayaInbox ${PACAYA_TAIKO}
         --taikoAnchor ${TAIKO_ANCHOR}
